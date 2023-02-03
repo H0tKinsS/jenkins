5fd33a9b481d703287dfc8107968cdfb5b1c316e
H0tKinsS <68612148+H0tKinsS@users.noreply.github.com>
2023-02-03 13:10:09 +0100

    Update main.tf
    ami

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
     region = "eu-central-1"
 }
 resource "aws_instance" "example_server" {
-    ami = "ami-ami-03e08697c325f02ab"
+    ami = "ami-03e08697c325f02ab"
     instance_type = "t2.micro"
 
     tags = {
